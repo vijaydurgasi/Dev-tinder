605dc94fbe13032aef5b7a8ebd30546a3e47b3d5
vijaydurgasi <vijayanu292157@gmail.com>
2026-02-27 11:38:55 +0530

    Handling multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -2,18 +2,56 @@ const express = require("express");
 
 const app = express();
 
-app.use("/contact", (req, res) => {
-    res.send("contacts are yet to update")
-});
+// app.use("/contact", (req, res) => {
+//     res.send("Yet to add...")
+// });
 
-app.use("/test", (req, res) => {
-    res.send("Testing")
-});
+// app.use("/test", (req, res) => {
+//     res.send("testing...")
+// });
 
-app.use("/", (req, res) => {
-    res.send("Namaste Developers...")
-});
+// app.use("/", (req, res) => {
+//     res.send("Namaste developers...")
+// });
 
-app.listen(9000, () => {
-    console.log("server created at port 9000...")
-})
+// app.get("/user/:UserId", (req, res) => {
+//     console.log(req.params)
+//     res.send({
+//         firstname: "vijay",
+//         lastname: "durgasi"
+//     })
+// });
+
+// app.post("/user", (req, res) => {
+//     res.send("Data has sucessfully posted")
+// });
+
+// app.delete("/user", (req, res) => {
+//     res.send("Data has sucessfully deleted")
+// });
+
+app.get("/user", (req, res, next) => {
+    // res.send("Response-1")
+    console.log("Response-1")
+    next();
+},
+    (req, res, next) => {
+        // res.send("Response - 2")
+        console.log("Response-2")
+        next();
+    },
+    (req, res, next) => {
+        // res.send("Response - 3")
+        console.log("Response-3")
+        next();
+    },
+    (req, res, next) => {
+        res.send("Respponse - 4")
+        console.log("Response-4")
+        // next();
+    },
+)
+
+app.listen(9000, () => [
+    console.log("server is running in 9000...")
+])
