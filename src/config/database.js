4bce9affadea53d19f3825136980c47a017f382e
vijaydurgasi <vijayanu292157@gmail.com>
2026-02-28 14:39:28 +0530

    connected to mongoDB

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect(
-        "mongodb://vijay:Vijay_89780@ac-ycdm767-shard-00-02.obavxw4.mongodb.net:27017/NamasteDB?tls=true&directConnection=true&authSource=admin"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;
