b2572c35298882050f6fb28ebddc450c450bf21c
vijaydurgasi <vijayanu292157@gmail.com>
2026-02-28 20:19:08 +0530

    Adding and accessing the Data from DB

--- src/app.js
+++ src/app.js
@@ -1,22 +1,16 @@
 require("dotenv").config();
-
 const express = require("express");
-
 const connectDB = require("./config/database");
-
-const User = require("./models/user")
-
+const User = require("./models/user");
 const app = express();
 app.use(express.json());
 
+//writing the data into Data base
 app.post("/signUp", async (req, res) => {
 
-    const user = new User({
-        firstName: "vijay",
-        lastName: "durgasi",
-        emailId: "vijay@gmail.com",
-        password: "Vijay@123"
-    });
+    // console.log(req.body);
+
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("User added successfully")
@@ -25,6 +19,31 @@ app.post("/signUp", async (req, res) => {
     }
 
 });
+
+//Get the user data from the data base
+app.get("/user", async (req, res) => {
+    const useremail = req.body.emailId;
+    try {
+        const user = await User.find({ emailId: useremail });
+        if (user.length === 0) {
+            res.status(404).send("user not found")
+        } else {
+            res.send(user)
+        }
+    } catch {
+        res.status(400).send("something went wrong")
+    }
+});
+
+//Get all the user data from the data base
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch {
+        res.status(400).send("something went wrong")
+    }
+})
 
 connectDB().then(() => {
     console.log("🚀 connection is successfull to data base")
