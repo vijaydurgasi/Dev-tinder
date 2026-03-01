bf89626688655ebd098c70fe4e9f77616a68ce2a
vijaydurgasi <vijayanu292157@gmail.com>
2026-03-01 21:50:08 +0530

    added validations

--- src/app.js
+++ src/app.js
@@ -57,14 +57,29 @@ app.delete("/deleteUser", async (req, res) => {
 })
 
 //Update the user data 
-app.patch("/updateUser", async (req, res) => {
+app.patch("/updateUser/:userId", async (req, res) => {
     const data = req.body;
+    const userId = req.params?.userId;
     try {
-        const userId = data.userId;
-        const updatedUser = await User.findByIdAndUpdate(userId, data, { new: true });
+        const ALLOWED_UPDATES = [
+            "firstName",
+            "lastName",
+            "about",
+            "gender",
+            "skills",
+            "age",
+        ];
+        const isUpadteAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+        if (!isUpadteAllowed) {
+            throw new Error("update not allowwed");
+        }
+        if (data.skills.length > 10) {
+            throw new Error("skills cannot be greater than 10");
+        }
+        const updatedUser = await User.findByIdAndUpdate(userId, data, { new: true, runValidators: true });
         res.send(updatedUser)
     } catch (err) {
-        res.status(400).send("something went wrong " + err.message)
+        res.status(400).send("upadte failed " + err.message)
     }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,37 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        require: true
+        require: true,
+        minlength: 3,
+        maxlength: 10
     },
     lastName: {
-        type: String
+        type: String,
+        minlength: 3,
+        maxlength: 10
     },
     emailId: {
         type: String,
         require: true,
         unique: true,
-        toLowerCase: true,
+        lowerCase: true,
         trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("In valid email address")
+            }
+        }
     },
     password: {
         type: String,
-        minLength: 8
+        minlength: 8
     },
     age: {
-        type: Number
+        type: Number,
+        maxlength: 2
     },
     gender: {
         type: String,
@@ -35,10 +46,24 @@ const userSchema = new mongoose.Schema({
     },
     about: {
         type: String,
-        default: "default about of the user"
+        default: "default about",
+        validate: {
+            validator: function (value) {
+                const wordCount = value.trim().split(/\s+/).length;
+                return wordCount <= 5;
+            },
+            message: "About section can have maximum 5 words"
+        }
     },
     skills: {
-        type: [String]
+        type: [String],
+        validate: {
+            validator: function (value) {
+
+                return value.length <= 5;
+            },
+            message: "skills section can have maximum 5 words"
+        }
     }
 }, { timestamps: true });
 
